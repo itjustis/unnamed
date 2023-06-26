7df5a4369204e0af996b0a389c6d136175aae379
itjustis <98639820+itjustis@users.noreply.github.com>
2023-06-26 16:57:35 +0700

    Update installthings.py

--- installthings.py
+++ installthings.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def install(package):
-    subprocess.check_call(["python", "-m", "pip", "install", "-q", package])
+    subprocess.check_call(["python", "--disable-pip-version-check","-m", "pip", "install", "-q", package])
 
 packages = ["diffusers==0.16.0","transformers","xformers","git+https://github.com/huggingface/accelerate.git","opencv-contrib-python","controlnet_aux","huggingface_hub","pyngrok","tomesd","clip-interrogator==0.6.0","scikit-image", "pytorch_lightning"]
     
