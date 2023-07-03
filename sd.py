827f55bdbc8c163ba9cb2ee4a4f811a6f1e525f0
itjustis <98639820+itjustis@users.noreply.github.com>
2023-07-04 03:15:07 +0700

    Update sd.py

--- sd.py
+++ sd.py
@@ -1,215 +1,340 @@
-from PIL import Image
+import torch
 import numpy as np
-import torch , os, gc, torch
-from diffusers.utils import load_image
-from transformers import ( pipeline, CLIPTokenizer, CLIPTextModel )
-from diffusers import (
-    DiffusionPipeline,
-    StableDiffusionPipeline,
-    StableDiffusionImg2ImgPipeline,
-    StableDiffusionControlNetPipeline,
-    AutoencoderKL,
-    UNet2DConditionModel,
-    ControlNetModel,
-    DDPMScheduler,
-    DDIMScheduler,
-    PNDMScheduler,
-    LMSDiscreteScheduler,
-    EulerDiscreteScheduler,
-    EulerAncestralDiscreteScheduler,
-    DPMSolverMultistepScheduler,
-    UniPCMultistepScheduler,
-)
-from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_controlnet import MultiControlNetModel
-from stable_diffusion_controlnet_img2img import StableDiffusionControlNetImg2ImgPipeline
-from huggingface_hub import snapshot_download
-from clip_interrogator import Config, Interrogator
-import tomesd
+from skimage import exposure
+from transformers import pipeline
+from torchvision.transforms import ToPILImage, ToTensor
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter, ImageDraw, ImageOps
+import cv2
+from controlnet_aux import  HEDdetector, ContentShuffleDetector, OpenposeDetector
 
-cnet_dict = {
-    'content': 'lllyasviel/control_v11f1e_sd15_tile',
-    'depth': 'lllyasviel/control_v11f1p_sd15_depth',
-    'scribble': 'lllyasviel/control_v11p_sd15_scribble',
-    'canny_edge': 'lllyasviel/control_v11p_sd15_canny',
-    'soft_edge': 'lllyasviel/control_v11p_sd15_softedge',
-    'shuffle': 'lllyasviel/control_v11e_sd15_shuffle',
-    'openpose': 'lllyasviel/control_v11p_sd15_openpose'
-}
+depth_estimator = pipeline('depth-estimation')
 
-samplers_dict={
-  'unipcm':UniPCMultistepScheduler,
-  'ddpm':DDPMScheduler, 
-  'ddim':DDIMScheduler,
-  'pndm':PNDMScheduler,
-  'lms':LMSDiscreteScheduler,
-  'euler_a':EulerAncestralDiscreteScheduler,
-  'euler':EulerDiscreteScheduler,
-  'dpm':DPMSolverMultistepScheduler
-}
+def cnet_prepare(controlnets,cnets_p, images, sz):
+	
+	for controlnet, prepare,image_path in zip(controlnets,cnets_p,images):
+		print ('prepare for', controlnet, 'is', prepare)
+		image = Image.open(image_path).resize(sz)
+
+		if prepare :
+			if controlnet == 'depth':
+				image = p_depth(image)
+			elif controlnet == 'tile':
+				image = p_tile(image, sz.size[0])
+			elif controlnet == 'canny_edge':
+				image = p_canny(image)
+			elif controlnet == 'soft_edge':
+				image = p_canny(image)
+				
+			print(image,'saving')
+			
+			image.resize(sz).convert('RGB').save(image_path)
+
+def p_openpose(image):
+	processor = OpenposeDetector.from_pretrained('lllyasviel/ControlNet')
+	control_image = processor(image, hand_and_face=True)
+	return control_image
+	
+def p_shuffle(image):
+	shuffle_processor = ContentShuffleDetector()
+	control_image = shuffle_processor(image)
+	return control_image
+	
+
+def p_soft(image):
+	processor = HEDdetector.from_pretrained('lllyasviel/Annotators')
+	control_image = processor(image, safe=True)
+	return control_image
+	
+def p_canny(image):
+	image = np.array(image)	
+	low_threshold = 100
+	high_threshold = 200	
+	image = cv2.Canny(image, low_threshold, high_threshold)
+	image = image[:, :, None]
+	image = np.concatenate([image, image, image], axis=2)
+	control_image = Image.fromarray(image)
+	return control_image
 
 
-def resize_for_condition_image(input_image: Image, resolution: int):
-    input_image = input_image.convert("RGB")
-    W, H = input_image.size
-    k = float(resolution) / min(H, W)
-    H *= k
-    W *= k
-    H = int(round(H / 64.0)) * 64
-    W = int(round(W / 64.0)) * 64
-    img = input_image.resize((W, H), resample=Image.LANCZOS)
-    return img
+def p_depth(init_image):
+	
 
-class SD:
-    def __init__(self, models_path, model_id, controlnet_model_id=None, torch_dtype=torch.float16, mo=True):
-        self.torch_dtype = torch_dtype
-        self.mo = mo
-        model_path = os.path.join(models_path,model_id)
-        self.model_path=model_path
-        self.cn_loaded=[]
-        
-        self.init_models(model_path)
-        self.clean()
-    
-    def interrogate(self, image,f=2,f2=4):
-        print('Interrogating.')
-      
-        if self.ci.interrogate:
-          return self.ci.interrogate(image,f,f2)
-        else:
-          self.ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
-          print('Interrogator loaded.')
-          return self.ci.interrogate(image,f,f2)
+	image = depth_estimator(init_image)['depth']
+	image = np.array(image)
+	image = image[:, :, None]
+	image = np.concatenate([image, image, image], axis=2)
+	control_image = Image.fromarray(image)
+	return control_image
 
-    def init_models(self, model_path):
-        print('initializing models.')
-        self.txt2img = StableDiffusionPipeline.from_pretrained(
-            model_path, torch_dtype=self.torch_dtype
-        ).to('cuda')
-        print('checkers')
-        self.txt2img.safety_checker=None
-        self.txt2img.feature_extractor=None
-        self.txt2img.requires_safety_checker=False
-        print('tome')
-        
-        tomesd.apply_patch(self.txt2img, ratio=0.5)
-        tomesd.apply_patch(self.txt2img, ratio=0.5)
-        
-        print('cn load')
-        
-        self.controlnet = StableDiffusionControlNetPipeline(vae=self.txt2img.vae,
-          text_encoder=self.txt2img.text_encoder,
-          tokenizer=self.txt2img.tokenizer,
-          unet=self.txt2img.unet,
-          controlnet=None,
-          scheduler=self.txt2img.scheduler,
-          safety_checker=None,
-          feature_extractor=None,
-          requires_safety_checker=False)
-          
-        print('cnok')
-        
-        ######
-        if self.mo:
-          self.txt2img.enable_xformers_memory_efficient_attention()
-          #self.txt2img.enable_model_cpu_offload()
-          
-        self.img2img = StableDiffusionImg2ImgPipeline(
-            vae=self.txt2img.vae,
-            text_encoder=self.txt2img.text_encoder,
-            tokenizer=self.txt2img.tokenizer,
-            unet=self.txt2img.unet,
-            scheduler=self.txt2img.scheduler,
-            safety_checker=None,
-            feature_extractor=None,
-            requires_safety_checker=False,
-        ).to('cuda')
-        
-        self.img2imgcontrolnet = StableDiffusionControlNetImg2ImgPipeline(
-            vae=self.txt2img.vae,
-            text_encoder=self.txt2img.text_encoder,
-            tokenizer=self.txt2img.tokenizer,
-            unet=self.txt2img.unet,
-            controlnet=None,
-            scheduler=self.txt2img.scheduler,
-            safety_checker=None,
-            feature_extractor=None,
-            requires_safety_checker=False,
-        ).to('cuda')
-        
-        print('loading sampler')
-        
-        self.load_sampler('euler_a')
-        
-        #### .enable_xformers_memory_efficient_attention()
+def p_tile(input_image: Image, resolution: int):
+	input_image = input_image.convert("RGB")
+	W, H = input_image.size
+	k = float(resolution) / min(H, W)
+	H *= k
+	W *= k
+	H = int(round(H / 64.0)) * 64
+	W = int(round(W / 64.0)) * 64
+	img = input_image.resize((W, H), resample=Image.LANCZOS)
+	return img
 
-        self.clean()
-        
-    def load_cnets(self, cnets, torch_dtype=torch.float16):
-      cnets_loaded = []
-      if self.cn_loaded != cnets:
-        for cnet in cnets:
-          if cnet in cnet_dict:
-              cnets_loaded.append(ControlNetModel.from_pretrained(cnet_dict[cnet], torch_dtype=torch_dtype).to('cuda'))
-          else:
-              print(cnet, 'not found')
-              
-        if len(cnets_loaded)==0:
-          self.controlnet.controlnet = None
-          self.img2imgcontrolnet.controlnet = self.controlnet.controlnet
-              
-        if len(cnets_loaded)==1:
-          self.controlnet.controlnet = cnets_loaded[0]
-          self.img2imgcontrolnet.controlnet = self.controlnet.controlnet
-          print('ok')
-          
-        if len(cnets_loaded)>1:
-          self.controlnet.controlnet = MultiControlNetModel(cnets_loaded)
-          self.img2imgcontrolnet.controlnet = self.controlnet
-          print('ok')
-      
-      self.cn_loaded = cnets
-    
-    def load_sampler(self,  sampler, torch_dtype=torch.float16):
-      print('loading sampler',sampler)
-      if sampler in samplers_dict:
-        self.txt2img.scheduler = samplers_dict[sampler].from_pretrained(self.model_path, subfolder="scheduler")
-        #from_config(self.txt2img.scheduler.config,torch_dtype=torch_dtype)
-        self.img2img.scheduler = self.txt2img.scheduler
-        self.controlnet.scheduler = self.txt2img.scheduler
-        self.img2imgcontrolnet.scheduler = self.txt2img.scheduler
-      else:
-        print('sampler '+sampler+' not found')
-      
-    
-    def load_model(self,model_id):
-      self.model_path = os.path.join(models_path,model_id)
-      self.txt2img.vae = AutoencoderKL.from_pretrained(self.model_path+'/vae').to('cuda')
-      self.txt2img.unet = UNet2DConditionModel.from_pretrained(self.model_path+'/unet').to('cuda')
-      self.txt2img.text_encoder = CLIPTextModel.from_pretrained(self.model_path+'/text_encoder').to('cuda')
-      self.txt2img.tokenizer = CLIPTokenizer.from_pretrained(self.model_path+'/tokenizer')
-      self.txt2img.scheduler = PNDMScheduler.from_pretrained(self.model_path+'/scheduler')
-      print ('loaded.')
-      self.clean()
-    
-    def clean(self):
-      gc.collect()
-      torch.cuda.empty_cache()
-      torch.cuda.ipc_collect()
-    
-    def load_controlnet(self,model):
-      return 'x'
-       
-    def annotate(self,image):
-        image = load_image(image)
-        image = depth_estimator(image)["depth"]
-        image = np.array(image)
-        image = image[:, :, None]
-        image = np.concatenate([image, image, image], axis=2)
-        image = Image.fromarray(image)
-        return image
 
-def download_models(models,models_path):
-  for model in models:
-    local_dir=os.path.join(models_path,model)    
-    print ('downloading '+model+' ...')
-    snapshot_download(repo_id=model, ignore_patterns=["*.msgpack", "*.safetensors", "*.ckpt"],local_dir=local_dir)
+def tile_upscale(image_path,upr,prompt,negative,pipe,controlnets,cn_scales,tile_size=768, shift=0.333,steps=25,scale=7.5,strength=0.666,interrogate=False):
+	img_upscaled , original_size , upscaled_size= upscale_image(image_path,upr)
+	img_upscaled = img_upscaled.convert('RGB')
+	result = process_tiles(pipe, controlnets, cn_scales, img_upscaled, original_size, prompt, negative, strength, tile_size, shift,steps,scale,interrogate)
+	return result
+
+def process_tiles(pipe, controlnets, cn_scales, img_upscaled, original_size, prompt, negative, strength, tile_size=768, shift=0.333,steps=25,scale=7.5, interrogate=False):
+	zz = 0
+
+	width, height = img_upscaled.size
+	x_steps = int(width // (tile_size * shift))+2
+	y_steps = int(height // (tile_size * shift))+2
+
+	print (img_upscaled.size)
+
+	for i in range(x_steps):
+		for j in range(y_steps):
+			#clear_output()
+
+			left = int(i * tile_size * shift)
+			upper = int(j * tile_size * shift)
+			right = left + tile_size
+			lower = upper + tile_size
+
+			if right <= width and lower <= height:
+				tile = img_upscaled.crop((left, upper, right, lower))
+
+				if interrogate:
+				  prompt=sd.interrogate(img_upscaled.resize((768,768)),2,4)
+				
+				#generator=torch.manual_seed(65),
+				
+				###need to fix
+				
+				condition_image = cnet_prepare(controlnets, tile)
+			
+				itile = tile
+
+				tile = pipe.img2imgcontrolnet(prompt=prompt,
+					  negative_prompt= negative,
+					  image=tile,
+					  controlnet_conditioning_image=condition_image,
+					  width=tile.size[0],
+					  height=tile.size[1],
+					  strength=strength,
+					  guidance_scale=scale,
+					  controlnet_conditioning_scale=cn_scales,
+					  num_inference_steps=steps,
+					  ).images[0]
+
+				tile = matchc(tile,itile)
+				img_upscaled.paste(tile, (left, upper), mask=ImageOps.invert(Image.open('tmask.png')).convert('L'))
+
+
+	img_processed = crop_image( img_upscaled, tile_size//6)
+
+	return img_processed
+
+
+def matchc(x,y):
+  remapped_np = np.array(x)
+  prev_np = np.array(y)
+  matched = color_match(remapped_np, np.array(x))
+  return Image.fromarray(matched)
+
+
+def add_border(image, border):
+	# Define the border color
+	color = (0, 0, 0)  # black
+
+	# Create a new image with a border
+	new_image = ImageOps.expand(image, border=border, fill=color)
+
+	return new_image
+
+
+def crop_image(image, crop_pixels):
+	width, height = image.size
+
+	# Define the cropping box - left, upper, right, lower
+	crop_box = (crop_pixels, crop_pixels, width - crop_pixels, height - crop_pixels)
+
+	# Create a new image by cropping the original image
+	new_image = image.crop(crop_box)
+
+	return new_image
+
+
+def upscale_image(image_path, upscale_factor=4, padding_size=768):
+	img = Image.open(image_path)
+	display(img)
+	width, height = img.size
+	new_size = (width * upscale_factor, height * upscale_factor)
+	img_upscaled = img.resize(new_size, Image.ANTIALIAS)
+
+
+	img_extended = add_border(img_upscaled, padding_size//3)
+
+	print('img_upscaled.size',img_upscaled.size,'. img_extended.size',img_extended.size)
+
+	return img_extended, (width, height), img_upscaled.size
+
+def color_match(prev_img,color_match_sample):
+  prev_img_lab = cv2.cvtColor(prev_img, cv2.COLOR_RGB2LAB)
+  color_match_lab = cv2.cvtColor(color_match_sample, cv2.COLOR_RGB2LAB)
+  matched_lab = exposure.match_histograms(prev_img_lab, color_match_lab, multichannel=True)
+  return cv2.cvtColor(matched_lab, cv2.COLOR_LAB2RGB)
+
+def add_noise(tensor, mean=0., std=1.):
+	"""
+	Add Gaussian noise to a tensor and clip values to valid range.
+
+	Args:
+	tensor (torch.Tensor): The input tensor.
+	mean (float): Mean of the Gaussian distribution to generate noise.
+	std (float): Standard deviation of the Gaussian distribution to generate noise.
+
+	Returns:
+	torch.Tensor: The tensor with added noise.
+	"""
+	noise = torch.randn_like(tensor) * std + mean
+	noisy_tensor = tensor + noise
+	noisy_tensor = torch.clamp(noisy_tensor, tensor.min(), tensor.max())
+	return noisy_tensor
+
+def create_gif(image_list, duration, output_path):
+	"""
+	Create a GIF from a list of PIL Images.
+
+	Args:
+	image_list (list): List of PIL Image objects.
+	duration (int): Duration between frames in the GIF (in milliseconds).
+	output_path (str): Path to save the output GIF.
+	"""
+	# Save the image list as a GIF
+	image_list[0].save(
+		output_path, save_all=True, append_images=image_list[1:], optimize=False, duration=duration, loop=0
+	)
+
+def tensor_to_pil(tensor):
+	"""
+	Convert a PyTorch tensor to a PIL Image.
+	"""
+	to_pil = ToPILImage()
+	return to_pil(tensor)
+
+def pil_to_tensor(image):
+	"""
+	Convert a PIL Image to a PyTorch tensor.
+	"""
+	to_tensor = ToTensor()
+	return to_tensor(image)
+
+def display_tensor_image(tensor):
+	"""
+	Display a PyTorch tensor as an image.
+	"""
+	plt.imshow(tensor_to_pil(tensor))
+	plt.show()
+
+def remap_displacement(image, displacement):
+	"""
+	Applies a displacement field (also known as remap) to an image.
+	`image` should be a PyTorch tensor with shape (C, H, W).
+	`displacement` should be a PyTorch tensor with shape (2, H, W) where
+	the first channel is the x displacement and the second channel is the y displacement.
+	"""
+	if isinstance(image, Image.Image):
+		image = pil_to_tensor(image)
+	elif isinstance(image, torch.Tensor):
+		pass
+	else:
+		raise ValueError('Image should be a PyTorch tensor or a PIL Image')
+	
+	image = image.to(displacement.device)
+
+	# Create a grid to apply the displacement
+	grid_y, grid_x = torch.meshgrid(
+		torch.linspace(-1, 1, image.shape[1]),
+		torch.linspace(-1, 1, image.shape[2]),
+	)
+
+	# Stack x and y grid components together
+	grid = torch.stack((grid_x, grid_y)).unsqueeze(0).to(displacement.device)
+
+	# Apply the displacement
+	grid += displacement
+
+	# Reshape the grid
+	grid = grid.permute(0, 2, 3, 1)
+
+	# Apply the grid to the image
+	#return torch.nn.functional.grid_sample(image.unsqueeze(0), grid, mode='bilinear', padding_mode='border', align_corners=True).squeeze(0)
+	return torch.nn.functional.grid_sample(image.unsqueeze(0), grid, mode='bilinear', padding_mode='reflection', align_corners=True).squeeze(0)
+
+
+def create_uv_map(image):
+	"""
+	Create a UV map with the same size as `image`.
+	`image` should be a PyTorch tensor with shape (C, H, W) or a PIL Image.
+	Returns a tensor with shape (2, H, W) where the first channel is the U map and the second channel is the V map.
+	"""
+	if isinstance(image, Image.Image):
+		# Convert PIL Image to tensor
+		image = pil_to_tensor(image)
+	elif isinstance(image, torch.Tensor):
+		pass
+	else:
+		raise ValueError('Image should be a PyTorch tensor or a PIL Image')
+
+	# Create a grid
+	grid = torch.meshgrid(
+		torch.linspace(0, 1, image.shape[1]),
+		torch.linspace(0, 1, image.shape[2]),
+	)
+	return torch.stack(grid)
+
+def process_displacement_image(displacement_image):
+	"""
+	Processes a displacement image to a displacement tensor.
+	Red channel is used as x displacement and green channel is used as y displacement.
+	"""
+	tensor = pil_to_tensor(displacement_image)
+	# Normalize displacement to [-1, 1] (assuming input is [0, 1])
+	tensor = tensor * 2 - 1
+	# Use only red and green channels for x and y displacement
+	return tensor[:2, :, :]
+
+def remap_displacement_depth(image, depth_map):
+	"""
+	Applies a displacement field (also known as remap) to an image using a depth map.
+	`image` should be a PyTorch tensor with shape (C, H, W).
+	`depth_map` should be a PyTorch tensor with shape (1, H, W).
+	"""
+	# Make sure the image and displacement are on the same device
+	image = image.to(depth_map.device)
+
+	# Create a grid to apply the displacement
+	grid_y, grid_x = torch.meshgrid(
+		torch.linspace(-1, 1, image.shape[1]), 
+		torch.linspace(-1, 1, image.shape[2]),
+	)
+
+	# Stack x and y grid components together
+	grid = torch.stack((grid_x, grid_y)).unsqueeze(0).to(depth_map.device)
+
+	# Normalize depth_map to range [-1,1] for displacement
+	depth_map = depth_map * 2 - 1
+
+	# Scale the displacement by the depth_map (this is a simple model and might not be perfect for all use cases)
+	grid += grid * depth_map
+
+	# Reshape the grid
+	grid = grid.permute(0, 2, 3, 1)
+
+	# Apply the grid to the image
+	return torch.nn.functional.grid_sample(image.unsqueeze(0), grid, mode='bilinear', padding_mode='reflection', align_corners=True).squeeze(0)
+
+
