74f5a73407c16a149104cb76bcd46c0f78674637
itjustis <98639820+itjustis@users.noreply.github.com>
2023-07-03 13:59:57 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,398 +1,194 @@
-import os, uuid, base64, argparse, subprocess , queue
-from io import BytesIO
-from threading import Thread
-from flask import Flask, request, jsonify
-from pyngrok import ngrok, conf
 from PIL import Image
-from IPython import display as disp
+import numpy as np
+import torch , os, gc, torch
+from diffusers.utils import load_image
+from transformers import ( pipeline, CLIPTokenizer, CLIPTextModel )
+from diffusers import (
+    DiffusionPipeline,
+    StableDiffusionPipeline,
+    StableDiffusionImg2ImgPipeline,
+    StableDiffusionControlNetPipeline,
+    AutoencoderKL,
+    UNet2DConditionModel,
+    ControlNetModel,
+    DDPMScheduler,
+    DDIMScheduler,
+    PNDMScheduler,
+    LMSDiscreteScheduler,
+    EulerDiscreteScheduler,
+    EulerAncestralDiscreteScheduler,
+    DPMSolverMultistepScheduler,
+    UniPCMultistepScheduler,
+)
 from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_controlnet import MultiControlNetModel
-from sd import SD, download_models
-from utils import cnet_prepare
+from stable_diffusion_controlnet_img2img import StableDiffusionControlNetImg2ImgPipeline
+from huggingface_hub import snapshot_download
+from clip_interrogator import Config, Interrogator
+import tomesd
 
-job_queue = queue.Queue()
-job_status = {}
+cnet_dict = {
+    'content': 'lllyasviel/control_v11f1e_sd15_tile',
+    'depth': 'lllyasviel/control_v11f1p_sd15_depth',
+    'scribble': 'lllyasviel/control_v11p_sd15_scribble',
+    'canny_edge': 'lllyasviel/control_v11p_sd15_canny',
+    'soft_edge': 'lllyasviel/control_v11p_sd15_softedge',
+    'shuffle': 'lllyasviel/control_v11e_sd15_shuffle'
+}
 
-import subprocess
-
-parser = argparse.ArgumentParser(description="Run Flask app with Ngrok")
-parser.add_argument("--token", type=str, help="Use Ngrok auth token")
-parser.add_argument("--models_path", type=str, default='/content/models/', help="Path to models directory")
-parser.add_argument("--log", action="store_true", help="log mode")
-
-app_args = parser.parse_args()
-
-
-models_path = app_args.models_path # set model path variable
-temp_folder = 'temp'
-if not os.path.exists(models_path):
-	os.makedirs(models_path)
-if not os.path.exists(temp_folder):
-	os.makedirs(temp_folder)
-
-available_models = [
-	#'CompVis/stable-diffusion-v1-4',
-	'stablediffusionapi/epicrealism'
-	#'runwayml/stable-diffusion-v1-5',
-	#'dreamlike-art/dreamlike-photoreal-2.0',
-	#''
-	]
-
-############# xxx ##############
-def clear():
-	disp.clear_output()
-def init(models):
-	download_models(models,models_path)
-	sd = SD( models_path, models[0], None)
-	return sd
-	
-sd = init(available_models)
-
-# Job queue
-job_queue = queue.Queue()
-
-# Job status dict
-job_status = {}
-
-# Available models list
-available_models = ['model1', 'model2', 'model3']
+samplers_dict={
+  'unipcm':UniPCMultistepScheduler,
+  'ddpm':DDPMScheduler, 
+  'ddim':DDIMScheduler,
+  'pndm':PNDMScheduler,
+  'lms':LMSDiscreteScheduler,
+  'euler_a':EulerAncestralDiscreteScheduler,
+  'euler':EulerDiscreteScheduler,
+  'dpm':DPMSolverMultistepScheduler
+}
 
 
-def save_image_from_b64(b64_string, folder, filename):
-	print(b64_string)
-	img_data = base64.urlsafe_b64decode(b64_string)
-	img_file_path = os.path.join(folder, filename)
-	
-	# Save the raw data to a file for debugging
-	with open(img_file_path + ".raw", "wb") as raw_file:
-		raw_file.write(img_data)
-	
-	try:
-		img = Image.open(BytesIO(img_data))
-		img_path = os.path.join(folder, filename)
-		img.save(img_path)
-		return img_path
-	except Exception as e:
-		print("Exception when trying to open image: ", e)
-		return None
+def resize_for_condition_image(input_image: Image, resolution: int):
+    input_image = input_image.convert("RGB")
+    W, H = input_image.size
+    k = float(resolution) / min(H, W)
+    H *= k
+    W *= k
+    H = int(round(H / 64.0)) * 64
+    W = int(round(W / 64.0)) * 64
+    img = input_image.resize((W, H), resample=Image.LANCZOS)
+    return img
 
-	
-def log(message):
-	if app_args.log:
-		print( str(message))
-		
-	
-from threading import Thread, Lock
+class SD:
+    def __init__(self, models_path, model_id, controlnet_model_id=None, torch_dtype=torch.float16, mo=True):
+        self.torch_dtype = torch_dtype
+        self.mo = mo
+        model_path = os.path.join(models_path,model_id)
+        
+        self.ci = Interrogator(Config(clip_model_name="ViT-L-14/openai"))
 
-def worker(lock):
-	while True:
-		with lock:
-			job = job_queue.get()
-			if job is None:
-				break
-			process_job(job)
+        self.init_models(model_path)
+        self.interrogate = self.ci.interrogate
+        self.clean()
 
-# Create a lock
-lock = Lock()
+    def init_models(self, model_path):
+        self.txt2img = StableDiffusionPipeline.from_pretrained(
+            model_path, torch_dtype=self.torch_dtype
+        ).to('cuda')
+        self.txt2img.safety_checker=None
+        self.txt2img.feature_extractor=None
+        self.txt2img.requires_safety_checker=False
+        
+        
+        
+        tomesd.apply_patch(self.txt2img, ratio=0.5)
+        
+        self.controlnet = StableDiffusionControlNetPipeline(vae=self.txt2img.vae,
+          text_encoder=self.txt2img.text_encoder,
+          tokenizer=self.txt2img.tokenizer,
+          unet=self.txt2img.unet,
+          controlnet=None,
+          scheduler=self.txt2img.scheduler,
+          safety_checker=None,
+          feature_extractor=None,
+          requires_safety_checker=False)
+        
+        ######
+        if self.mo:
+          self.txt2img.enable_xformers_memory_efficient_attention()
+          #self.txt2img.enable_model_cpu_offload()
+          
+        self.img2img = StableDiffusionImg2ImgPipeline(
+            vae=self.txt2img.vae,
+            text_encoder=self.txt2img.text_encoder,
+            tokenizer=self.txt2img.tokenizer,
+            unet=self.txt2img.unet,
+            scheduler=self.txt2img.scheduler,
+            safety_checker=None,
+            feature_extractor=None,
+            requires_safety_checker=False,
+        ).to('cuda')
+        
+        self.img2imgcontrolnet = StableDiffusionControlNetImg2ImgPipeline(
+            vae=self.txt2img.vae,
+            text_encoder=self.txt2img.text_encoder,
+            tokenizer=self.txt2img.tokenizer,
+            unet=self.txt2img.unet,
+            controlnet=None,
+            scheduler=self.txt2img.scheduler,
+            safety_checker=None,
+            feature_extractor=None,
+            requires_safety_checker=False,
+        ).to('cuda')
+        
+        self.load_sampler('euler_a')
+        
+        #### .enable_xformers_memory_efficient_attention()
 
-# Start the worker thread
-worker_thread = Thread(target=worker, args=(lock,))
-worker_thread.start()
+        
+        
+          
+        self.clean()
+        
+    def load_cnets(self, cnets, torch_dtype=torch.float16):
+      cnets_loaded = []
+      for cnet in cnets:
+        if cnet in cnet_dict:
+            cnets_loaded.append(ControlNetModel.from_pretrained(cnet_dict[cnet], torch_dtype=torch_dtype).to('cuda'))
+        else:
+            print(cnet, 'not found')
+            
+      if len(cnets_loaded)==0:
+        self.controlnet.controlnet = None
+        self.img2imgcontrolnet.controlnet = self.controlnet.controlnet
+            
+      if len(cnets_loaded)==1:
+        self.controlnet.controlnet = cnets_loaded[0]
+        self.img2imgcontrolnet.controlnet = self.controlnet.controlnet
+        print('ok')
+        
+      if len(cnets_loaded)>1:
+        self[pipe].controlnet = MultiControlNetModel(cnets_loaded)
+        print('ok')
+    
+    def load_sampler(self,  sampler, torch_dtype=torch.float16):
+      if sampler in samplers_dict:
+        self.txt2img.scheduler = samplers_dict[sampler].from_config(pipe.scheduler.config,torch_dtype=torch_dtype).to('cuda')
+        self.img2img.scheduler = self.txt2img.scheduler
+        self.controlnet.scheduler = self.txt2img.scheduler
+        self.img2imgcontrolnet.scheduler = self.txt2img.scheduler
+      else:
+        print('sampler '+sampler+' not found')
+      
+    
+    def load_model(self,model_id):
+      self.model_path = os.path.join(models_path,model_id)
+      self.txt2img.vae = AutoencoderKL.from_pretrained(self.model_path+'/vae').to('cuda')
+      self.txt2img.unet = UNet2DConditionModel.from_pretrained(self.model_path+'/unet').to('cuda')
+      self.txt2img.text_encoder = CLIPTextModel.from_pretrained(self.model_path+'/text_encoder').to('cuda')
+      self.txt2img.tokenizer = CLIPTokenizer.from_pretrained(self.model_path+'/tokenizer')
+      self.txt2img.scheduler = PNDMScheduler.from_pretrained(self.model_path+'/scheduler')
+      print ('loaded.')
+      self.clean()
+    
+    def clean(self):
+      gc.collect()
+      torch.cuda.empty_cache()
+      torch.cuda.ipc_collect()
+    
+    def load_controlnet(self,model):
+      return 'x'
+       
+    def annotate(self,image):
+        image = load_image(image)
+        image = depth_estimator(image)["depth"]
+        image = np.array(image)
+        image = image[:, :, None]
+        image = np.concatenate([image, image, image], axis=2)
+        image = Image.fromarray(image)
+        return image
 
-
-# init
-if app_args.token:
-	conf.get_default().auth_token =  app_args.token
-##
-public_url = ngrok.connect(5000)
-
-app = Flask(__name__)
-app.debug = False
-
-
-@app.route('/api/info/status', methods=['GET'])
-def status():
-	# TODO: Return the status of jobs in queue, server idleness/workload, and GPU/RAM data
-	pass
-
-@app.route('/api/info/models', methods=['GET'])
-def models():
-	return jsonify(available_models)
-
-@app.route('/api/job/delete', methods=['DELETE'])
-def delete_job():
-	job_id = request.args.get('jobid')
-	if job_id and job_id in job_status:
-		job_status[job_id]['status'] = 'deleted'
-		return jsonify({"result": "Job deleted"})
-	else:
-		return jsonify({"error": "Job not found"}), 404
-
-
-@app.route('/api/job/<path:job_id>/status', methods=['GET'])
-def get_job_status(job_id):
-	#log(job_id)
-	if job_id and job_id in job_status:
-		return jsonify(job_status[job_id])
-	else:
-		return jsonify({"error": "Job not found"}), 404
-
-
-
-# List of allowed tasks.
-ALLOWED_TASKS = ['imagine', 'overpaint', 'inpaint', 'controlnet']
-
-@app.route('/api/<path:task>', methods=['POST'])
-def create_task(task):
-	# Verify if the task is valid.
-	if task not in ALLOWED_TASKS:
-		return jsonify({"error": "Invalid task"}), 400
-
-	# Fetch request data.
-	job_data = request.get_json()
-	args = job_data['args']
-	job_id = job_data['id']
-
-	# Log request data for debugging.
-	log(f"Task: {task}")
-	
-
-	img_path = None
-
-	# Process the image if the task is not 'imagine'.
-	if task != 'imagine':
-		img_path = process_image(args, job_id)
-		
-	cnet_images=[]
-	for cnet in args['modules']:
-		filename = f"temp/{cnet}.png";
-		if args['modules'][cnet]['ref']:
-			if args['modules'][cnet]['ref']['image']:
-				filename = process_cnet_image(args['modules'][cnet]['ref']['image'],filename)
-				cnet_images.append(filename)
-		else:
-			Image.open(img_path).save(filename)
-			cnet_images.append(filename)
-			
-	args['cnet_images'] = cnet_images;
-
-	# Create a job object and put it in the queue.
-	job = create_job(args, job_id, img_path, task)
-
-	# Update the job status and return the response.
-	job_status[job_id] = {"job_id": job_id, "status": "queued"}
-
-	return jsonify(job_status[job_id])
-
-def process_cnet_image(b64_string,filename):
-	"""Decode the image from base64 and save it."""
-	
-	f = BytesIO()
-	f.write(base64.b64decode(b64_string))
-	f.seek(0)
-	
-	img = Image.open(f)
-	
-	#if args.get('inpaint') != "true":
-	#	img = img.convert("RGB")
-	
-	
-	img.save(filename)
-	
-	return filename
-	
-def process_image(args, job_id):
-	"""Decode the image from base64 and save it."""
-	b64_string = args['initImage']
-	f = BytesIO()
-	f.write(base64.b64decode(b64_string))
-	f.seek(0)
-
-	img = Image.open(f)
-
-	if args.get('inpaint') != "true":
-		img = img.convert("RGB")
-
-	filename = f"temp/{job_id}.png"
-	img.save(filename)
-
-	return filename
-
-def create_job(args, job_id, img_path, task):
-	"""Create a job object."""
-	args['img_path'] = str(img_path)
-	job = {
-		"job_id": job_id,
-		"task": task,
-		"status": "queued",
-		"args": args
-	}
-	job_queue.put(job)
-
-	return job
-
-# Helper function to convert PIL image to base64
-def image_to_base64(img):
-	buffered = BytesIO()
-	img.save(buffered, format="PNG")
-	return base64.b64encode(buffered.getvalue()).decode("utf-8")
-	
-# SD functions
-def imagine(args):
-	log ('imagining')
-	sz = (args['width'],args['height'])
-	image = Image.open(args['img_path']).convert('RGB').resize(sz)
-	
-	if len(args['modules']) > 0:
-		log ('with control')
-		cnet_images=args['cnet_images']
-		cnets,cscales,cnets_p = cnetmodules (args['modules'])
-		
-		sd.load_cnets(cnets)
-		
-		if(variation==0):
-			cnet_prepare(cnets_,cnets_p,cnet_images,sz)
-			
-		cnet_image_pils = []
-		
-		for img in cnet_images:
-			cnet_image_pils.append(Image.open(img));
-		
-		return sd.controlnet(
-			args['prompt'],
-			controlnet_conditioning_image=cnet_image_pils,
-			num_inference_steps=int(args['steps']),
-			guidance_scale=float(args['scale']),
-			negative_prompt=args['negative_prompt'],
-			strength=float(args['strength']),
-			controlnet_conditioning_scale=cscales
-		)[0][0]
-		
-	else:
-		return sd.txt2img(
-			args['prompt'],
-			width=args['width'],
-			height=args['height'],
-			num_inference_steps=int(args['steps']),
-			guidance_scale=float(args['scale']),
-			negative_prompt=args['negative_prompt']
-		)[0][0]
-
-def cnetmodules(modules):
-	cnets_p = []
-	cnets = []
-	cscales = []
-	
-	if len(modules) == 1:
-		for cnet in args['modules']:
-			cnets.append ( str(modules[cnet]['mode']) )
-			cscales = (float(modules[cnet]['scale']))
-			cnets_p.append(modules[cnet]['prepare'])
-					
-	else:	
-		for cnet in modules:
-			cnets.append((str(args['modules'][cnet]['mode'])))
-			cscales.append(float(args['modules'][cnet]['scale']))
-			cnets_p.append(args['modules'][cnet]['prepare'])
-			
-	return (cnets,cscales,cnets_p)
-	
-
-def overpaint(args,variation):
-	log ('overpainting with image at '+args['img_path'])
-	sz = (args['width'],args['height'])
-	image = Image.open(args['img_path']).convert('RGB').resize(sz)
-	
-	if len(args['modules']) > 0:
-		cnet_images=args['cnet_images']
-		cnets,cscales,cnets_p = cnetmodules (args['modules'])
-		
-		sd.load_cnets(cnets)
-		
-		if(variation==0):
-			cnet_prepare(cnets_,cnets_p,cnet_images,sz)
-			
-		cnet_image_pils = []
-		
-		for img in cnet_images:
-			cnet_image_pils.append(Image.open(img));
-		
-		return sd.img2imgcontrolnet(
-			args['prompt'],
-			image,
-			controlnet_conditioning_image=cnet_image_pils,
-			num_inference_steps=int(args['steps']),
-			guidance_scale=float(args['scale']),
-			negative_prompt=args['negative_prompt'],
-			strength=float(args['strength']),
-			controlnet_conditioning_scale=cscales
-		)[0][0]
-	else:
-		return sd.img2img(
-			args['prompt'],
-			image,
-			num_inference_steps=int(args['steps']),
-			guidance_scale=float(args['scale']),
-			negative_prompt=args['negative_prompt'],
-			strength=float(args['strength'])
-		)[0][0]
-	
-def inpaint(args):
-	return None
-	
-def controlnet(args):
-	return None
-	
-# Function to process jobs
-def process_job(job):
-	try:
-		task = job['task']
-		args = job['args']
-		job_id = job["job_id"]
-		job_status[job_id]['status'] = "processing"
-		variations = int(args['variations'])
-		
-		result = None
-		b64_result = ''
-		divider = ''
-			
-		log('variations: '+str(variations))
-		
-		if args['prompt'] == "":
-			image = Image.open(args['img_path']).convert('RGB')
-			args['prompt'] = sd.interrogate(image, min_flavors=2, max_flavors=4)
-			
-			
-		for i in range(variations):
-			if variations>1 and i!=(variations-1):
-					   divider = ','
-			else:
-					   divider = ''
-			log('generating image #'+str(i))
-			if task == 'imagine':
-				result = imagine(args)
-			elif task == 'overpaint':
-				result = overpaint(args,i)
-			elif task == 'inpaint':
-				result = inpaint(args)
-			elif task == 'controlnet':
-				result = controlnet(args)
-				
-			log('saving result')
-				
-			result.save(temp_folder+'/out_'+str(i)+'_'+job_id+'.png')
-				
-			b64_result+=image_to_base64(result.convert('RGB'))+divider
-	
-		if result is not None:
-			
-			job_status[job['job_id']] = {"status": "completed", "result": b64_result}
-		else:
-			job_status[job['job_id']] = {"status": "failed"}
-	except Exception as e:
-		print(f"Error processing job {job['job_id']}: {e}")
-		job_status[job['job_id']] = {"status": "failed", "error": str(e)}
-
-if __name__ == "__main__":
-	clear()
-	log(public_url)
-	app.run()
+def download_models(models,models_path):
+  for model in models:
+    local_dir=os.path.join(models_path,model)    
+    print ('downloading '+model+' ...')
+    snapshot_download(repo_id=model, ignore_patterns=["*.msgpack", "*.safetensors", "*.ckpt"],local_dir=local_dir)
