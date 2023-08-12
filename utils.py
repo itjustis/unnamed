60787eb3cb8f22d4d6a28debb26fd66029da1ebe
itjustis <98639820+itjustis@users.noreply.github.com>
2023-08-12 09:42:34 +0700

    sssd

--- utils.py
+++ utils.py
@@ -128,7 +128,8 @@ def process_tiles(pipe, controlnets, cn_scales, img_upscaled, original_size, pro
 				###need to fix
 				tile.save('temp_tile_pre.png')
 				
-				condition_image = cnet_prepare(controlnets, [True], ['temp_tile_pre.png'],[tile_size,tile_size])[0]
+				condition_image = Image.open(cnet_prepare(controlnets, [True], ['temp_tile_pre.png'],[tile_size,tile_size]))
+				#print(condition_image)
 			
 				itile = tile
 
